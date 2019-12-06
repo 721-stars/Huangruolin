7675dde92375ac0898a02e3646bd48fed2339db4
721-stars <57714775+721-stars@users.noreply.github.com>
2019-12-06 12:24:22 +0800

    Add files via upload

---
+++ sortedArrayToBST.py
@@ -0,0 +1,57 @@
+import json
+import ast
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if nums:
+            m = len(nums) // 2
+            r = TreeNode(nums[m])
+            r.left, r.right = map(self.sortedArrayToBST, [nums[:m], nums[m + 1:]])
+            return r
+
+
+def treeNodeToString(root):
+    if not root:
+        return "[]"
+    output = ""
+    queue = [root]
+    current = 0
+    while current != len(queue):
+        node = queue[current]
+        current = current + 1
+
+        if not node:
+            output += "null, "
+            continue
+
+        output += str(node.val) + ", "
+        queue.append(node.left)
+        queue.append(node.right)
+    return "[" + output[:-2] + "]"
+
+
+def main():
+    line = list(ast.literal_eval(input("输入列表，以逗号隔开：")))
+    while True:
+        try:
+            ret = Solution().sortedArrayToBST(line)
+            out = treeNodeToString(ret)
+            print(out)
+            break
+        except StopIteration:
+            break
+
+
+if __name__ == '__main__':
+    main()
